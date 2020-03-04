70677a645a39768f9f2ddb365a58277cb587a9d6
Long Dinh <tronglongcntt@gmail.com>
2020-03-04 08:13:36 +0700

    add println! sample

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    //default 31 becomes an i32
+    println!("{} days", 31);
+
+    //position of arguments can be used
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    //named arguments
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+
+    //special formatting can be specified after a `:`.
+    println!("{0} in {0:b} binary format", 2);
 }
