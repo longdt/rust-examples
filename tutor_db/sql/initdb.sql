712a6104431e1cd2be92a258fe1ad03a974b93ef
Long Dinh <tronglongcntt@gmail.com>
2024-02-24 16:21:26 +0700

    add sqlx example

---
+++ tutor_db/sql/initdb.sql
@@ -0,0 +1,7 @@
+create table course
+(
+    course_id   bigserial primary key,
+    tutor_id    bigint       not null,
+    course_name varchar(200) not null,
+    created_at  timestamptz default CURRENT_TIMESTAMP
+);
---
+++ tutor_db/src/bin/iter1.rs
@@ -0,0 +1,42 @@
+use std::{env, io};
+
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::types::time::OffsetDateTime;
+
+#[derive(Debug)]
+pub struct Course {
+    pub course_id: i64,
+    pub tutor_id: i64,
+    pub course_name: String,
+    pub created_at: Option<OffsetDateTime>,
+}
+
+#[ntex::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .unwrap();
+    let courses = sqlx::query!("select course_id, tutor_id, course_name, created_at from course where course_id = $1",
+    1
+    )
+        .fetch_all(&db_pool)
+        .await
+        .unwrap();
+    let mut course_list = vec![];
+    for course in courses {
+        course_list.push(Course {
+            course_id: course.course_id,
+            tutor_id: course.tutor_id,
+            course_name: course.course_name,
+            created_at: course.created_at
+        })
+    }
+    println!("Courses = {:?}", course_list);
+    Ok(())
+}
