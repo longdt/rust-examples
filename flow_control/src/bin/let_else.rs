5e2ecd974bbb6bf6bbe6d61dc67fea69a23e8b75
Long Dinh <tronglongcntt@gmail.com>
2024-04-08 09:14:45 +0700

    let else need be diverging function

--- flow_control/src/bin/let_else.rs
+++ flow_control/src/bin/let_else.rs
@@ -3,7 +3,10 @@ use std::str::FromStr;
 fn get_count_item(s: &str) -> (u64, &str) {
     let mut it = s.split(' ');
     let (Some(count_str), Some(item)) = (it.next(), it.next()) else {
+        println!("Can't segment count item pair: '{s}'");
         panic!("Can't segment count item pair: '{s}'");
+        // TODO ^ Try comment this line
+        // Error! expected `!`, found `()`
     };
     let Ok(count) = u64::from_str(count_str) else {
         panic!("Can't parse integer: '{count_str}'");
@@ -13,5 +16,5 @@ fn get_count_item(s: &str) -> (u64, &str) {
 
 fn main() {
     assert_eq!(get_count_item("3 chairs"), (3, "chairs"));
-    assert_eq!(get_count_item("3a chairs"), (3, "chairs"));
+    assert_eq!(get_count_item("3chairs"), (3, "chairs"));
 }
